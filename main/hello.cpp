e1678fa4bbd231bb083c0a06c6f738e82720a5b3
Rayan <munzernouman@gmail.com>
2023-02-15 00:16:41 +0530

    this is the commit for CC subject using Jenkins

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout<<"This is cloud computing assignment here !!";
+  cout<<"PES University , Bangalore...";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
